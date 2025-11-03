1f50bba5173ee4667c877940f1940da31cb08a5d
= <mukhtarrahimi.dev@gmail.com>
2025-11-03 20:28:52 +0430

    code debug

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
+const PostSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
