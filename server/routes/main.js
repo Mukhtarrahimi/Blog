37df55292235c97525886a0d23f877b9ccedbae6
= <mukhtarrahimi.dev@gmail.com>
2025-11-03 20:24:53 +0430

    insertDate to datebase

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
 router.get('', (req, res) => {
   locals = {
@@ -9,6 +10,20 @@ router.get('', (req, res) => {
   res.render('index', { locals });
 });
 
+const insertData = () => {
+  Post.insertMany([
+    {
+      title: 'post 1',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, eaque! Perspiciatis ',
+    },
+    {
+      title: 'post 2',
+      body: 'Perspiciatis saepe sit totam. Explicabo facilis incidunt mollitia inventore',
+    },
+  ]);
+};
+
+insertData();
 router.get('/about', (req, res) => {
   res.render('about');
 });
