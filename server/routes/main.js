2f73dc5980159173b6d39e843f0939ccbc468d82
= <mukhtarrahimi.dev@gmail.com>
2025-11-03 21:32:11 +0430

    paggination

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,13 +3,28 @@ const router = express.Router();
 const Post = require('../models/Post');
 
 router.get('', async (req, res) => {
-  locals = {
-    title: 'Node js ',
-    description: 'This is a Node js project',
-  };
   try {
-    const posts = await Post.find({});
-    res.render('index', { locals, posts });
+    locals = {
+      title: 'Node js ',
+      description: 'This is a Node js project',
+    };
+    let perPage = 6;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+    const count = await Post.count();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render('index', {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
   } catch (err) {
     console.log(err);
   }
