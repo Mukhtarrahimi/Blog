d9c582e49e81050e329a2a71be5ab36fb494127b
= <mukhtarrahimi.dev@gmail.com>
2025-11-05 11:07:25 +0430

    admin.js code

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
+const adminLayout = require('../views/layouts/admin');
+
+router.get('/admin', async (req, res) => {
+  try {
+    const locals = {
+      title: 'Admin',
+      body: 'Simple Blog created with NodeJs, Express & MongoDb',
+    };
+    res.render('/index/admin', {
+      locals,
+      layout: adminLayout,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
