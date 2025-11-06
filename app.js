7ffb2d78cf2e1ff04858af52e32109679b92d335
= <mukhtarrahimi.dev@gmail.com>
2025-11-06 19:40:15 +0430

    cookie-parser import to app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const app = express();
 const connectDB = require('./server/config/db');
