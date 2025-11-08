365282a4d24551d930986453baf43452acd7c310
= <mukhtarrahimi.dev@gmail.com>
2025-11-08 21:44:27 +0430

    authMiddleware

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -7,73 +7,114 @@ const jwt = require('jsonwebtoken');
 
 const adminLayout = '../views/layouts/admin';
 const jwtSecret = process.env.JWT_SECRET;
+
+/**
+ *
+ * Check Login
+ */
+const authMiddleware = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+};
+
+/**
+ * GET /
+ * Admin - Login Page
+ */
 router.get('/admin', async (req, res) => {
   try {
     const locals = {
       title: 'Admin',
-      body: 'Simple Blog created with NodeJs, Express & MongoDb',
+      description: 'Simple Blog created with NodeJs, Express & MongoDb.',
     };
-    res.render('admin/index', {
-      locals,
-      layout: 'layouts/admin',
-    });
-  } catch (err) {
-    console.log(err);
+
+    res.render('admin/index', { locals, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
   }
 });
 
-// login
+/**
+ * POST /
+ * Admin - Check Login
+ */
 router.post('/admin', async (req, res) => {
   try {
     const { username, password } = req.body;
-    if (username === 'admin' && password === 'password') {
-      res.send('you are logind...');
-    } else {
-      res.send('your password or username is wrong.');
-    }
-  } catch (err) {
-    console.log(err);
-  }
-});
 
-router.post('/admin', async (req, res) => {
-  try {
-    const { username, password } = req.body;
     const user = await User.findOne({ username });
+
     if (!user) {
-      return res.status(401).json({ message: 'Invalid creadentials' });
+      return res.status(401).json({ message: 'Invalid credentials' });
     }
+
     const isPasswordValid = await bcrypt.compare(password, user.password);
+
     if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid creadentials' });
+      return res.status(401).json({ message: 'Invalid credentials' });
     }
+
     const token = jwt.sign({ userId: user._id }, jwtSecret);
     res.cookie('token', token, { httpOnly: true });
     res.redirect('/dashboard');
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    console.log(error);
   }
 });
 
-router.get('/dashboard', async (req, res) => {
-  res.render('admin/dashboard');
+/**
+ * GET /
+ * Admin Dashboard
+ */
+router.get('/dashboard', authMiddleware, async (req, res) => {
+  try {
+    const locals = {
+      title: 'Dashboard',
+      description: 'Simple Blog created with NodeJs, Express & MongoDb.',
+    };
+
+    const data = await Post.find();
+    res.render('admin/dashboard', {
+      locals,
+      data,
+      layout: adminLayout,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
-// Register
+
+/**
+ * POST /
+ * Admin - Register
+ */
 router.post('/register', async (req, res) => {
   try {
     const { username, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
+
     try {
       const user = await User.create({ username, password: hashedPassword });
       res.status(201).json({ message: 'User Created', user });
-    } catch (err) {
-      if (err.code === 11000) {
+    } catch (error) {
+      if (error.code === 11000) {
         res.status(409).json({ message: 'User already in use' });
       }
       res.status(500).json({ message: 'Internal server error' });
     }
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    console.log(error);
   }
 });
 
