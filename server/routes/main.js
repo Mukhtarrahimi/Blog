6ec75d1830fa94ea681dab02faf6a2a56e2479a9
= <mukhtarrahimi.dev@gmail.com>
2025-11-13 15:43:16 +0430

    header code

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('', (req, res) => {
-  const local = {
+  locals = {
     title: 'Node js ',
     description: 'This is a Node js project',
   };
-  res.render('index', { title: 'Home', local });
+  res.render('index', { locals });
 });
 
 router.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
+  res.render('about');
 });
 module.exports = router;
